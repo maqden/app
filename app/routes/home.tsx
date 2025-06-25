a2888f690649dfdc5a983a7838b008c658c4312a
Renato Dehnhardt <renatotkd@gmail.com>
2025-06-25 10:02:04 +0200

    feat: remove logo assets and welcome page
    - Deleted dark and light logo SVG files as they are no longer needed.
    - Removed `welcome.tsx` component and its associated functionality since the welcome page is no longer required.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,17 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import {Button} from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    {title: "New React Router App"},
+    {name: "description", content: "Welcome to React Router!"},
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button>HOME</Button>
+    </div>
+  );
 }
